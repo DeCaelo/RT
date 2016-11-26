d7570059e7dbd3985071f4ca97c64859eb0701c3
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-11-26 16:03:20 +0100

    setup textinput component

---
+++ components/TextInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class TextInput extends Component {
+
+  render() {
+    return <div>This is the TextInput Component :) </div>
+  }
+
+}
+
+export default TextInput
